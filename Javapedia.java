b2c1de6d5a941da056203f9f55dc0a5ee5ff5e25
s15236 <s15236@pjwstk.edu.pl>
2023-02-26 16:46:17 +0100

    Initial commit

---
+++ Javapedia.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Javapedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n**********Javapedia**********");
+        System.out.println("How many historical figures will you register?");
+
+        System.out.println("These are the values you stored:"); 
+        //Task 4: call print2DArray.
+
+        System.out.print("\nWho do you want information on? ");  
+        
+
+
+        scan.close();
+    }
+
+    /**
+     * Function name: print2DArray
+     * @param array (String[][])
+     *
+     * Inside the function
+     *  1. print the database
+     *     • a tab of space precedes each row.
+     *     • each value in database has one space from the other value.
+     *     • print a new line.
+     */
+
+}
